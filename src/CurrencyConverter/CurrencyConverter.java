8094c40a3c2d5536c3deb1460a0c72d5eaf328c4
DimaBog22 <bogdanov111222@yandex.ru>
2021-06-29 21:52:30 +0300

    added Currency Converter task

---
+++ src/CurrencyConverter/CurrencyConverter.java
@@ -0,0 +1,39 @@
+package CurrencyConverter;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class CurrencyConverter {
+
+    public static void main(String[] args) {
+
+        double [] valueOfCurrency = {0.39, 0.33, 28.77};
+        String [] nameOfCurrency = {"USD", "EUR", "RUB"};
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите сумму в BYN");
+        int number = sc.nextInt();
+
+        Scanner sc2 = new Scanner(System.in);
+        System.out.println("Введите валюту (USD, EUR, RUB)");
+        String name = sc2.nextLine();
+
+        double result = 0.0;
+        String finalResult = "";
+
+        for (int i = 0; i < valueOfCurrency.length; i++) {
+
+            if (name.equals(nameOfCurrency[i])) {
+
+                result = number * valueOfCurrency[i];
+                finalResult = nameOfCurrency[i];
+
+                System.out.println(result + " " + finalResult);
+
+            }
+
+        }
+
+    }
+
+}
