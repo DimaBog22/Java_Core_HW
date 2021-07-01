4836f22b90cefa5b8dca0f52a65f4c17c6665d7b
DimaBog22 <bogdanov111222@yandex.ru>
2021-07-01 19:27:29 +0300

    created steve monk

--- src/Classes/Monk.java
+++ src/Classes/Monk.java
@@ -2,6 +2,37 @@ package Classes;
 
 public class Monk {
 
+    public short id;
+    public int age;
+    public double experience;
 
+    public double kick() {
+
+        double damage = Math.random();
+        return damage;
+
+    }
+
+    public void speak(String word) {
+        System.out.println("Monk is speaking " + word);
+    }
+
+}
+
+class Executor {
+
+    public static void main(String[] args) {
+
+        Monk steve = new Monk();
+        steve.age = 10;
+        System.out.println(steve.age);
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println(steve.kick());
+        }
+
+        steve.speak("Hi low");
+
+    }
 
 }
