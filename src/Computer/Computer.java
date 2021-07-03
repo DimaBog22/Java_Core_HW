3889068c2927ebfeb1ee86e3d8b98b05cf1e8d1f
DimaBog22 <bogdanov111222@yandex.ru>
2021-07-03 18:38:41 +0300

    implemented the whole logic of class Computer

--- src/Computer/Computer.java
+++ src/Computer/Computer.java
@@ -1,4 +1,100 @@
 package Computer;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Computer {
+
+    public String cpu;
+    public String ram;
+    public String hdd;
+    public int workCycle = 0;
+    public int workCycleLimit = 3;
+    boolean isWorking = true;
+
+    Computer(String cpu, String ram, String hdd) {
+
+        this.cpu = cpu;
+        this.ram = ram;
+        this.hdd = hdd;
+
+    }
+
+    public void print() {
+        System.out.println("cpu: " + cpu + ", ram: " + ram + ", hdd: " + hdd +
+                ", work cycles: " + workCycle + ", Limit: " + workCycleLimit);
+    }
+
+    public void switchOn() {
+
+//      метод включить, при включении может произойти сбой, при вызове метода
+//      рандом загадывает число (0 либо 1), вы вводите число с клавиатуры, если
+//      угадали, комп включается, если нет -  сгорает.Если комп сгорел при попытке
+//      включить, нужно выдать сообщение что ему конец
+
+        if (workCycle >= workCycleLimit) {
+
+            System.out.println("Количество циклов работы " + workCycle + " превышает лимит циклов работы " + workCycleLimit);
+            System.out.println("Купите новый комп");
+
+        } else if (!(workCycle >= workCycleLimit) && isWorking == true){
+
+            Random random = new Random();
+            int randomNumber = random.nextInt(2);
+            String message = "Компьютер сгорел при попытке включения";
+
+            System.out.println("Введите число 1 или 0:");
+            Scanner sc = new Scanner(System.in);
+            int number = sc.nextInt();
+
+                if (number == randomNumber) {
+
+                    System.out.println("Добро пожаловать");
+                    workCycle++;
+
+                } else {
+
+                    System.out.println(message);
+                    isWorking = false;
+
+                }
+        }
+
+    }
+
+    public void switchOff() {
+
+        if (workCycle >= workCycleLimit) {
+
+            System.out.println("Количество циклов работы " + workCycle + " превышает лимит циклов работы " + workCycleLimit);
+            System.out.println("Купите новый комп");
+
+        } else if (workCycle > 1 && workCycle < workCycleLimit && isWorking == true) { // проверка на то, что нельзя нельзя выключить комп, если он не включен
+
+            Random random = new Random();
+            int randomNumber = random.nextInt(2);
+            String message = "Компьютер сгорел при попытке включения";
+
+            System.out.println("Введите число 1 или 0:");
+            Scanner sc = new Scanner(System.in);
+            int number = sc.nextInt();
+
+                if (number == randomNumber) {
+
+                    System.out.println("Всего доброго");
+                    workCycle++;
+
+                } else {
+
+                    System.out.println(message);
+                    isWorking = false;
+
+                }
+
+        } else {
+            System.out.println("Вы не можете выключить компьютер, тк он не включен или сгорел");
+        }
+
+    }
+
 }
--- src/Computer/Main.java
+++ src/Computer/Main.java
@@ -1,4 +1,18 @@
 package Computer;
 
 public class Main {
+
+    public static void main(String[] args) {
+
+        Computer pc = new Computer("core i5", "16gb", "2tb");
+
+        pc.print();
+        pc.switchOff();
+        pc.switchOn();
+        pc.print();
+        pc.switchOff();
+
+
+    }
+
 }
