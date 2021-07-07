e8ee9da923f5bbade8df22d93dd80ed558460dab
DimaBog22 <bogdanov111222@yandex.ru>
2021-07-07 19:14:47 +0300

    added the second line of inheritance (AirTransport). Added constructors to parent and child classes

--- src/Transport/CivilTransport.java
+++ src/Transport/CivilTransport.java
@@ -1,8 +1,13 @@
 package Transport;
 
-public class CivilTransport {
+public class CivilTransport extends AirTransport{
 
     public int passengers;
     public boolean isBusiness;
 
+    public CivilTransport(int capacity, int maxSpeed, int weight, String brand, int wingspan, int minRunwayLength, int passengers, boolean isBusiness) {
+        super(capacity, maxSpeed, weight, brand, wingspan, minRunwayLength);
+        this.passengers = passengers;
+        this.isBusiness = isBusiness;
+    }
 }
--- src/Transport/MilitaryTransport.java
+++ src/Transport/MilitaryTransport.java
@@ -1,8 +1,13 @@
 package Transport;
 
-public class MilitaryTransport {
+public class MilitaryTransport extends AirTransport{
 
     public boolean isCatapult;
     public int rockets;
 
+    public MilitaryTransport(int capacity, int maxSpeed, int weight, String brand, int wingspan, int minRunwayLength, boolean isCatapult, int rockets) {
+        super(capacity, maxSpeed, weight, brand, wingspan, minRunwayLength);
+        this.isCatapult = isCatapult;
+        this.rockets = rockets;
+    }
 }
