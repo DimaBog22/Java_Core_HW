29144833cc0b6399b0c141e9ccddf7e0753eb0a0
DimaBog22 <bogdanov111222@yandex.ru>
2021-07-07 19:11:12 +0300

    added the second line of inheritance (LandTransport). Added constructors to parent and child classes

--- src/Transport/FreightTransport.java
+++ src/Transport/FreightTransport.java
@@ -1,7 +1,11 @@
 package Transport;
 
-public class FreightTransport {
+public class FreightTransport extends LandTransport{
 
     public int liftingCapacity;
 
+    public FreightTransport(int capacity, int maxSpeed, int weight, String brand, int wheels, int fuelConsumption, int liftingCapacity) {
+        super(capacity, maxSpeed, weight, brand, wheels, fuelConsumption);
+        this.liftingCapacity = liftingCapacity;
+    }
 }
--- src/Transport/LightTransport.java
+++ src/Transport/LightTransport.java
@@ -1,8 +1,13 @@
 package Transport;
 
-public class LightTransport {
+public class LightTransport extends LandTransport{
 
     public String type;
     public int passengers;
 
+    public LightTransport(int capacity, int maxSpeed, int weight, String brand, int wheels, int fuelConsumption, String type, int passengers) {
+        super(capacity, maxSpeed, weight, brand, wheels, fuelConsumption);
+        this.type = type;
+        this.passengers = passengers;
+    }
 }
