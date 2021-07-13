6055bf8cd9ca042ee51da8f42f14b09597435e6c
DimaBog22 <bogdanov111222@yandex.ru>
2021-07-13 17:59:13 +0300

    Overrided putOn and takeOff methods, added private field nameOfShoes into RunningShoes and WorkingShoes classes

--- src/Сlothes/Footwear/RunningShoes.java
+++ src/Сlothes/Footwear/RunningShoes.java
@@ -1,4 +1,22 @@
 package Сlothes.Footwear;
 
-public class RunningShoes {
+public class RunningShoes implements Footwear{
+
+    private String nameOfShoes;
+
+    RunningShoes (String name) {
+        this.nameOfShoes = name;
+    }
+
+    @Override
+    public void putOn() {
+        System.out.println(nameOfShoes + " одеты");
+    }
+
+
+    @Override
+    public void takeOff() {
+        System.out.println(nameOfShoes + " сняты");
+    }
+
 }
--- src/Сlothes/Footwear/WorkingShoes.java
+++ src/Сlothes/Footwear/WorkingShoes.java
@@ -1,4 +1,23 @@
 package Сlothes.Footwear;
 
-public class WorkingShoes {
+public class WorkingShoes implements Footwear{
+
+    private String nameOfShoes;
+
+    WorkingShoes (String name) {
+        this.nameOfShoes = name;
+    }
+
+    @Override
+    public void putOn() {
+        System.out.println(nameOfShoes + " одеты");
+    }
+
+
+    @Override
+    public void takeOff() {
+        System.out.println(nameOfShoes + " сняты");
+    }
+
+
 }
