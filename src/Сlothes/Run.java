5d0fbae514ea99cf2153fe444c390c52aec52c28
DimaBog22 <bogdanov111222@yandex.ru>
2021-07-13 18:39:18 +0300

    created some objects for testing

--- src/Сlothes/Run.java
+++ src/Сlothes/Run.java
@@ -8,7 +8,29 @@ public class Run {
 
     public static void main(String[] args) {
 
-        RunningShoes nike = new RunningShoes("nike");
+        Footwear nike = new RunningShoes("nike");
+        Footwear timberland = new WorkingShoes("timberland");
+
+        Jacket carhartt = new Coat("carhartt");
+        Jacket nb = new Windbreaker("new balance");
+
+        Trousers calvinKlein = new Pants("calvin klein");
+        Trousers levis = new Jeans("levi's");
+
+        Human mike = new Human("Mike", nike,
+                carhartt, calvinKlein);
+        Human carl = new Human("Carl", timberland,
+                nb, levis);
+
+
+        mike.putOn();
+        mike.takeOff();
+
+        carl.putOn();
+        carl.takeOff();
+
+
+
 
     }
 
