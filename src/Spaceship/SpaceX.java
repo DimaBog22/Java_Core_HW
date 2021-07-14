d0bdba80d9a187d340bf3e2da861b36361ecb0db
DimaBog22 <bogdanov111222@yandex.ru>
2021-07-14 17:26:53 +0300

    added class SpaceX implemented IStart

---
+++ src/Spaceship/SpaceX.java
@@ -0,0 +1,31 @@
+package Spaceship;
+
+import java.util.Random;
+
+public class SpaceX implements IStart{
+
+    @Override
+    public boolean preCheck() {
+
+        Random random = new Random();
+        int number = random.nextInt(10);
+
+        if (number > 3) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
+    @Override
+    public void launch() {
+        System.out.println("Двигатели SpaceX запущены. Все системы в норме");
+    }
+
+    @Override
+    public void start() {
+        System.out.println("Старт SpaceX");
+    }
+
+}
