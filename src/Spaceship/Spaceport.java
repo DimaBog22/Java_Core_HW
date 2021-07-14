557183b95862e4b2f01a1f75013aa7835f758d2c
DimaBog22 <bogdanov111222@yandex.ru>
2021-07-14 17:11:54 +0300

    added new logic to method lauch

--- src/Spaceship/Spaceport.java
+++ src/Spaceship/Spaceport.java
@@ -7,7 +7,15 @@ public class Spaceport {
         if (item.preCheck() == false) {
             System.out.println("Предстартовая проверка провалена");
         } else {
+
             item.launch();
+
+            for (int i = 1; i <= 10; i++) {
+                System.out.println(i);
+            }
+
+            item.start();
+
         }
 
     }
