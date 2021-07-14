1000e40eafd3474743827a139d416bee0cd7d83f
DimaBog22 <bogdanov111222@yandex.ru>
2021-07-14 17:19:18 +0300

    added class Shuttle

---
+++ src/Spaceship/Shuttle.java
@@ -0,0 +1,31 @@
+package Spaceship;
+
+import java.util.Random;
+
+public class Shuttle implements IStart {
+
+    @Override
+    public boolean preCheck() {
+
+        Random random = new Random();
+        int number = random.nextInt(10);
+
+        if (number > 3) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
+    @Override
+    public void launch() {
+        System.out.println("вигатели Шатла запущены. Все системы в норме");
+    }
+
+    @Override
+    public void start() {
+        System.out.println("Старт Шатла");
+    }
+
+}
