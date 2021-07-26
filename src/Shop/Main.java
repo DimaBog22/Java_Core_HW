2544a3e237ef5d475faaef63c0a1adbde6cfe7f0
DimaBog22 <bogdanov111222@yandex.ru>
2021-07-26 12:39:53 +0300

    testing od deleteItem method

--- src/Shop/Main.java
+++ src/Shop/Main.java
@@ -4,9 +4,15 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Item adidasEasy = new Item(11, "adidas easy", 300);
+        Item adidasEasy = new Item(11, "adidas easy352", 200);
+        Item adidasEasy2 = new Item(12, "adidas easy584", 300);
         Shop pobedy15 = new Shop();
         pobedy15.addItem(adidasEasy);
+        pobedy15.addItem(adidasEasy2);
+
+        System.out.println(pobedy15.getListOfItems());
+
+        pobedy15.deleteItem(adidasEasy);
 
         System.out.println(pobedy15.getListOfItems());
 
